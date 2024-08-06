81866acf08313576d5c26f81b8a037bae7df55ea
CM Takahashi Toshimitsu <takahashi.toshimitsu@classmethod.jp>
2024-08-06 15:12:17 +0900

    implement home screen logic

--- user_app/chalicelib/assets/main.js
+++ user_app/chalicelib/assets/main.js
@@ -101,15 +101,104 @@ const mmSummaryTable = new DataTable('#mmSummaryTable', {
     }
 });
 
-function loadData() {
-    mmDataTable.clear().rows.add(deviceDataItems).draw();
+function setupFilteringMmDataForm(codeTables, dataItems) {
+    const menuSelect = document.querySelector('select[name="menu"]')
+    Object.entries(codeTables.menus).forEach(([val, name]) => {
+        const opt = document.createElement('option');
+        opt.textContent = name;
+        opt.value = val;
+        menuSelect.appendChild(opt);
+    });
+
+    const modeSelect = document.querySelector('select[name="mode"]')
+    Object.entries(codeTables.modes).forEach(([val, name]) => {
+        const opt = document.createElement('option');
+        opt.textContent = name;
+        opt.value = val;
+        modeSelect.appendChild(opt);
+    });
+
+    const periodRadios = document.querySelectorAll('input[name="period"]');
+    const rangeStartDate = document.querySelector('input[name="rangeStartDate"]');
+    const rangeEndDate = document.querySelector('input[name="rangeEndDate"]');
+
+    const startDate = new Date(dataItems[0].timestamp * 1000).toLocaleDateString('sv-SE');
+    const endDate = new Date(dataItems.at(-1).timestamp * 1000).toLocaleDateString('sv-SE');
+
+    rangeStartDate.min = rangeEndDate.min = startDate;
+    rangeStartDate.max = rangeEndDate.max = endDate;
+    rangeStartDate.value = startDate;
+    rangeEndDate.value = endDate;
+
+    const updateDateFieldsState = () => {
+        const selectedValue = document.querySelector('input[name="period"]:checked')?.value;
+        const isRangeSelected = selectedValue === 'range';
+
+        rangeStartDate.disabled = !isRangeSelected;
+        rangeEndDate.disabled = !isRangeSelected;
+    
+        [rangeStartDate, rangeEndDate].forEach(input => {
+            input.parentElement.classList.toggle('disabled', !isRangeSelected);
+        });
+    }
+
+    periodRadios.forEach(radio => {
+        radio.addEventListener('change', updateDateFieldsState);
+    });
+
+    // 測定データ絞り込みボタンの処理
+    document.getElementById('filterForm').onsubmit = () => {
+        const form = document.getElementById('filterForm');
+        if (form.reportValidity()) {
+            if (form.menu.value === 'all' && form.mode.value === 'all' && form.period.value === 'all') {
+                loadData(dataItems);
+            } else {
+                const minTs = new Date(form.rangeStartDate.value + 'T00:00:00').getTime() / 1000;
+                const maxTs = new Date(form.rangeEndDate.value + 'T23:59:59').getTime() / 1000;
+                const latestCnt = form.mode.value === 0 ? 20 : 80;
+
+                const results = dataItems.filter((item, idx) => {
+                    if (form.menu.value != 'all') {
+                        if (item.menu !== Number(form.menu.value)) return false;
+                    }
+                    if (form.mode.value != 'all') {
+                        if (item.mode !== Number(form.mode.value)) return false;
+                    }
+                    if (form.period.value === 'range') {
+                        if (item.timestamp < minTs) return false;
+                        if (item.timestamp > maxTs) return false;
+                    } else if (form.period.value === 'latest') {
+                        if (idx >= latestCnt) return false;
+                    }
+                    return true;
+                });
+                loadData(results);
+            }
+        }
+        return false;
+    };
+
+    updateDateFieldsState(); // 初期化処理
+}
+
+function loadData(dataItems) {
+    const mmDataItems = dataItems
+        .filter(item => item.mmCode !== undefined)
+        .sort((a, b) => {
+            return b.timestamp - a.timestamp; // 測定日時
+        });
+    mmDataTable.clear().rows.add(mmDataItems).draw();
 
     const mmCodeCountMap = {}
-    for (const item of deviceDataItems) {
+    for (const item of dataItems) {
         const curVal = mmCodeCountMap[item.mmCode];
         mmCodeCountMap[item.mmCode] = curVal === undefined ? 1 : curVal + 1;
     }
-    const summary = Object.entries(mmCodeCountMap).sort((a, b) => b[1] - a[1])
+    const summary = Object.entries(mmCodeCountMap).sort((a, b) => {
+            const r = b[1] - a[1]; // 出現回数降順
+            if (r === 0) return a[0] - b[0]; // 測定コード昇順
+            return r;
+        })
         .map(([mmCode, count], index) => {
             return {
                 id: index + 1,
@@ -120,4 +209,14 @@ function loadData() {
     mmSummaryTable.clear().rows.add(summary).draw();
 }
 
-loadData();
+// 初期化処理
+document.addEventListener('DOMContentLoaded', function() {
+    setupFilteringMmDataForm(window.codeTables, window.deviceDataItems);
+
+    // メッセージ生成ボタンの処理
+    document.getElementById('generateMessageButton').onclick = () => {
+    };
+
+    // 初期ロード
+    loadData(window.deviceDataItems);
+});
